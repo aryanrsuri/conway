b4af99dc4cd218e2e50ebf54c0e65734dabfd9ad
aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>
2024-07-11 21:31:56 -0700

    Some refactors of functions, random is now a parameter
    Signed-off-by: aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>

--- src/conway.zig
+++ src/conway.zig
@@ -1,48 +1,9 @@
 const std = @import("std");
-pub const CELL_COUNT = 400;
+
+// TODO: Must be abstracted out
+pub const CELL_COUNT = 900;
 pub const sq = std.math.sqrt(CELL_COUNT);
 pub const Cells = [CELL_COUNT]u1;
-
-pub fn debug(cells: *Cells, title: []const u8) !void {
-    try std.io.getStdOut().writer().writeAll("\x1B[2J\x1B[H");
-    try std.io.getStdOut().writer().writeAll("\n\t\tConway's Game of Life ");
-    try std.io.getStdOut().writer().print("(GEN={any})", .{title[0]});
-    try std.io.getStdOut().writer().writeAll("\n\t  ");
-    var i: usize = 0;
-    while (i < CELL_COUNT) : (i += 1) {
-        const cell = get(cells, i);
-        if (cell == 0) {
-            // Red for 0
-            try std.io.getStdOut().writer().writeAll("\x1B[31m");
-        } else {
-            // Blue for 1
-            try std.io.getStdOut().writer().writeAll("\x1B[34m");
-        }
-        try std.io.getStdOut().writer().print("{d} ", .{cell});
-        // Reset color
-        try std.io.getStdOut().writer().writeAll("\x1B[0m");
-
-        if ((i + 1) % sq == 0) {
-            try std.io.getStdOut().writer().writeAll("\n\t  ");
-        }
-    }
-    try std.io.getStdOut().writer().writeAll("\n");
-    // try std.io.getStdOut().flush();
-
-    std.time.sleep(std.time.ns_per_s / 10);
-}
-
-pub fn randomise(cells: *Cells) void {
-    var prng = std.Random.DefaultPrng.init(@intCast(std.time.milliTimestamp()));
-    for (cells, 0..) |_, c| {
-        const bit = prng.random().float(f64);
-        if (bit < 0.5) {
-            mask(cells, c, 0);
-        } else {
-            mask(cells, c, 1);
-        }
-    }
-}
 
 pub fn check(cells: *Cells, pos: usize) void {
     const offsets = [8]isize{ -sq - 1, -sq, -sq + 1, -1, 1, sq - 1, sq, sq + 1 };
@@ -58,20 +19,86 @@ pub fn check(cells: *Cells, pos: usize) void {
 
     if (live) {
         if (count == 2 or count == 3) return;
-        set(cells, pos);
+        mask(cells, pos);
     } else if (count == 3) {
-        set(cells, pos);
+        mask(cells, pos);
     }
 }
 
-pub fn set(cells: *Cells, pos: usize) void {
+pub fn mask(cells: *Cells, pos: usize) void {
     cells.*[pos] = ~cells.*[pos];
 }
 
-pub fn mask(cells: *Cells, pos: usize, bit: u1) void {
+pub fn set(cells: *Cells, pos: usize, bit: u1) void {
     cells.*[pos] = bit;
 }
 
 pub fn get(cells: *Cells, pos: usize) u1 {
     return cells.*[pos];
 }
+
+pub fn debug(cells: *Cells, title: []const u8) !void {
+    try std.io.getStdOut().writer().writeAll("\x1B[2J\x1B[H");
+    try std.io.getStdOut().writer().writeAll("\n\tConway's Game of Life ");
+    if (title.len == 1) {
+        try std.io.getStdOut().writer().print("(GEN={any})", .{title[0]});
+    } else {
+        try std.io.getStdOut().writer().print("\n\t{s}", .{title});
+    }
+    try std.io.getStdOut().writer().writeAll("\n\t  ");
+    var i: usize = 0;
+    while (i < CELL_COUNT) : (i += 1) {
+        const cell = get(cells, i);
+        if (cell == 0) {
+            // Red for 0
+            try std.io.getStdOut().writer().writeAll("\x1B[31m");
+        } else {
+            // Blue for 1
+            try std.io.getStdOut().writer().writeAll("\x1B[34m");
+        }
+        try std.io.getStdOut().writer().print("{d} ", .{cell});
+        try std.io.getStdOut().writer().writeAll("\x1B[0m");
+
+        if ((i + 1) % sq == 0) {
+            try std.io.getStdOut().writer().writeAll("\n\t  ");
+        }
+    }
+    try std.io.getStdOut().writer().writeAll("\n");
+    std.time.sleep(std.time.ns_per_s / 15);
+}
+
+pub fn randomise(cells: *Cells) void {
+    var prng = std.Random.DefaultPrng.init(@intCast(std.time.milliTimestamp()));
+    for (cells, 0..) |_, c| {
+        const bit = prng.random().float(f64);
+        if (bit < 0.5) {
+            set(cells, c, 0);
+        } else {
+            set(cells, c, 1);
+        }
+    }
+}
+
+/// Play the standard Conway's Game of Life for N generations
+/// @param  cells: *Cells -> Bit Array Representing the Cell Automatae
+/// @param  generations: u8 -> N generations (Max 225 for printing)
+/// @param  random: bool -> Randomise the cell state before playing?
+pub fn play(cells: *Cells, generations: u8, random: bool) !void {
+    if (random) randomise(cells);
+    var i: usize = 0;
+    while (i < generations) : (i += 1) {
+        for (cells, 0..) |_, c| {
+            check(cells, c);
+        }
+        _ = try debug(cells, &[_]u8{@intCast(i)});
+    }
+}
+
+pub const Conway = struct {
+    count: usize,
+    square: usize = std.math.sqrt(CELL_COUNT),
+    cells: [CELL_COUNT]u1,
+    pub fn init(file: []const u8) void {
+        std.debug.print("{s}\n", .{file});
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,19 @@
 const std = @import("std");
 const con = @import("conway.zig");
-
 pub fn main() !void {
-    // Max to keep the GEN able to pass into []const u8
-    const GEN = 256;
-    var cells: con.Cells = [_]u1{1} ** con.CELL_COUNT;
-    con.randomise(&cells);
-    var i: usize = 0;
-    while (i < GEN) : (i += 1) {
-        for (cells, 0..) |_, c| {
-            con.check(&cells, c);
-        }
-        std.debug.print("\n", .{});
-        _ = try con.debug(&cells, &[_]u8{@intCast(i)});
-    }
-}
-test "Randomise" {
     var cells: con.Cells = [_]u1{0} ** con.CELL_COUNT;
-    con.randomise(&cells);
-    con.debug(&cells, "Conway") catch {};
+    _ = try con.play(&cells, 255, true);
+}
+
+test "toad" {
+    // Won't work , grid dependent, will patch
+    var cells: con.Cells = [_]u1{0} ** con.CELL_COUNT;
+    con.set(&cells, 21, 1);
+    con.set(&cells, 22, 1);
+    con.set(&cells, 29, 1);
+    con.set(&cells, 41, 1);
+    con.set(&cells, 48, 1);
+    con.set(&cells, 49, 1);
+    _ = try con.debug(&cells, "Toad");
+    // _ = try con.play(&cells, 1, false);
 }
